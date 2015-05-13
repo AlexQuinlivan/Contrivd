761aa4ad50ba66389b86b42972b8fcca87383675
Alex Quinlivan <alex@quinlivan.co.nz>
2015-05-14 10:14:53 +1200

    Initial commit

---
+++ Contrivd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Contrivd
+//
+//  Created by Alex Quinlivan on 14/05/15.
+//  Copyright (c) 2015 Alex Quinlivan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ContrivdTests/ContrivdTests.m
@@ -0,0 +1,40 @@
+//
+//  ContrivdTests.m
+//  ContrivdTests
+//
+//  Created by Alex Quinlivan on 14/05/15.
+//  Copyright (c) 2015 Alex Quinlivan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ContrivdTests : XCTestCase
+
+@end
+
+@implementation ContrivdTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
